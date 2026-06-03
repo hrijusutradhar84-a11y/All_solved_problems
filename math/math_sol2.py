328f90faa46c16775e153263b58bdbbcf0f38190
hrijusutradhar84-a11y <hrijusutradhar84@gmail.com>
2026-06-03 06:39:34 +0530

    Add math_sol2 permutation helper

---
+++ math/math_sol2.py
@@ -0,0 +1,28 @@
+'''
+In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+
+Examples:
+
+With input 'a':
+Your function should return: ['a']
+
+With input 'ab':
+Your function should return ['ab', 'ba']
+
+With input 'abc':
+Your function should return ['abc','acb','bac','bca','cab','cba']
+
+With input 'aabb':
+Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+Note: The order of the permutations doesn't matter.
+
+Good luck!
+'''
+
+from itertools import permutations as i_permu
+
+
+def permutations(s):
+    return list(set(''.join(p) for p in i_permu(s)))
